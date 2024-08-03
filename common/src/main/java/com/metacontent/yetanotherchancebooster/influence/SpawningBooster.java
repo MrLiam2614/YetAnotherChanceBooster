d8bb0a9e7633c8b521a4769344fae6108eef5fef
gatekeep06 <mvmetacontent@gmail.com>
2024-08-03 18:17:14 +0300

    add abstract parent booster class

---
+++ common/src/main/java/com/metacontent/yetanotherchancebooster/influence/SpawningBooster.java
@@ -0,0 +1,56 @@
+package com.metacontent.yetanotherchancebooster.influence;
+
+import com.cobblemon.mod.common.api.spawning.SpawnBucket;
+import com.cobblemon.mod.common.api.spawning.context.SpawningContext;
+import com.cobblemon.mod.common.api.spawning.context.calculators.SpawningContextCalculator;
+import com.cobblemon.mod.common.api.spawning.detail.SpawnAction;
+import com.cobblemon.mod.common.api.spawning.detail.SpawnDetail;
+import com.cobblemon.mod.common.api.spawning.influence.SpawningInfluence;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class SpawningBooster implements SpawningInfluence {
+    protected final PlayerEntity player;
+
+    public SpawningBooster(PlayerEntity player) {
+        this.player = player;
+    }
+
+    @Override
+    public void affectAction(@NotNull SpawnAction<?> spawnAction) {
+
+    }
+
+    @Override
+    public boolean isExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean affectSpawnable(@NotNull SpawnDetail spawnDetail, @NotNull SpawningContext spawningContext) {
+        return true;
+    }
+
+    @Override
+    public float affectWeight(@NotNull SpawnDetail spawnDetail, @NotNull SpawningContext spawningContext, float v) {
+        return v;
+    }
+
+    @Override
+    public void affectSpawn(@NotNull Entity entity) {
+
+    }
+
+    @Override
+    public float affectBucketWeight(@NotNull SpawnBucket spawnBucket, float v) {
+        return v;
+    }
+
+    @Override
+    public boolean isAllowedPosition(@NotNull ServerWorld serverWorld, @NotNull BlockPos blockPos, @NotNull SpawningContextCalculator<?, ?> spawningContextCalculator) {
+        return true;
+    }
+}
