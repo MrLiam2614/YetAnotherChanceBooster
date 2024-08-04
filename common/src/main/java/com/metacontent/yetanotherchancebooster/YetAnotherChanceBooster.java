cdc091f0712141ea711be9e65ace10f5339819c3
gatekeep06 <mvmetacontent@gmail.com>
2024-08-04 15:57:36 +0300

    remove status effect

--- common/src/main/java/com/metacontent/yetanotherchancebooster/YetAnotherChanceBooster.java
+++ common/src/main/java/com/metacontent/yetanotherchancebooster/YetAnotherChanceBooster.java
@@ -1,13 +1,10 @@
 package com.metacontent.yetanotherchancebooster;
 
 import com.cobblemon.mod.common.api.spawning.spawner.PlayerSpawnerFactory;
-import com.metacontent.yetanotherchancebooster.effect.ShinyBoostEffect;
 import com.metacontent.yetanotherchancebooster.influence.ShinyBooster;
 
 public final class YetAnotherChanceBooster {
     public static final String MOD_ID = "yetanotherchancebooster";
-
-    public static final ShinyBoostEffect SHINY_BOOST_EFFECT = new ShinyBoostEffect();
 
     public static void init() {
         PlayerSpawnerFactory.INSTANCE.getInfluenceBuilders().add(ShinyBooster::new);
--- fabric/src/main/java/com/metacontent/yetanotherchancebooster/fabric/YetAnotherChangeBoosterFabric.java
+++ fabric/src/main/java/com/metacontent/yetanotherchancebooster/fabric/YetAnotherChangeBoosterFabric.java
@@ -1,19 +1,12 @@
 package com.metacontent.yetanotherchancebooster.fabric;
 
-import com.metacontent.yetanotherchancebooster.effect.ShinyBoostEffect;
 import net.fabricmc.api.ModInitializer;
 
 import com.metacontent.yetanotherchancebooster.YetAnotherChanceBooster;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
 public final class YetAnotherChangeBoosterFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        Registry.register(Registries.STATUS_EFFECT, new Identifier(YetAnotherChanceBooster.MOD_ID, "shiny_boost_effect"),
-                YetAnotherChanceBooster.SHINY_BOOST_EFFECT);
-
         YetAnotherChanceBooster.init();
     }
 }
