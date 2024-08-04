7e086b661ce3cdc8d20f7a27b346d70cbb65e877
gatekeep06 <mvmetacontent@gmail.com>
2024-08-04 16:05:20 +0300

    register new boosters

--- common/src/main/java/com/metacontent/yetanotherchancebooster/YetAnotherChanceBooster.java
+++ common/src/main/java/com/metacontent/yetanotherchancebooster/YetAnotherChanceBooster.java
@@ -1,12 +1,16 @@
 package com.metacontent.yetanotherchancebooster;
 
 import com.cobblemon.mod.common.api.spawning.spawner.PlayerSpawnerFactory;
+import com.metacontent.yetanotherchancebooster.influence.LabelWeightBooster;
 import com.metacontent.yetanotherchancebooster.influence.ShinyBooster;
+import com.metacontent.yetanotherchancebooster.influence.SpeciesWeightBooster;
 
 public final class YetAnotherChanceBooster {
     public static final String MOD_ID = "yetanotherchancebooster";
 
     public static void init() {
         PlayerSpawnerFactory.INSTANCE.getInfluenceBuilders().add(ShinyBooster::new);
+        PlayerSpawnerFactory.INSTANCE.getInfluenceBuilders().add(SpeciesWeightBooster::new);
+        PlayerSpawnerFactory.INSTANCE.getInfluenceBuilders().add(LabelWeightBooster::new);
     }
 }
